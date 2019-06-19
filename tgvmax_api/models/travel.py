0b6d216efb0b7da5561809497b3e84d30aac908b
TeddyTrqt <turquet.teddy@gmail.com>
2019-06-19 21:55:02 +0200

    fix error: moving travel to modelscd

--- tgvmax_api/models/travel.py
+++ tgvmax_api/models/travel.py
@@ -1,59 +1,60 @@
 from tgvmax_api.models.station import Station
+from tgvmax_api.models.travel_offer import TravelOffer
 
 class Travel:
 
     def __init__(self, data={}):
-        self.data = data
+        self._data = data
 
     @property
     def origin(self) -> Station:
         "Return station object of travel origin"
-        if 'origin' not in self.data:
+        if 'origin' not in self._data:
             raise ValueError('No origin key in travel data')
-        if 'station' not in self.data['origin']:
+        if 'station' not in self._data['origin']:
             raise ValueError('No station key in travel data[origin]')
-        if 'metaData' not in self.data['origin']['station']:
+        if 'metaData' not in self._data['origin']['station']:
             raise ValueError('No metaData key in travel data[origin][station]')
-        if 'MI' not in self.data['origin']['station']['metaData']:
+        if 'MI' not in self._data['origin']['station']['metaData']:
             raise ValueError('No MI key in travel data[origin][station][metaData]')
-        if 'code' not in self.data['origin']['station']['metaData']['MI']:
+        if 'code' not in self._data['origin']['station']['metaData']['MI']:
             raise ValueError('No MI key in travel data[origin][station][metaData][MI]')
-        return Station(self.data['origin']['station']['metaData']['MI']['code'])
+        return Station(self._data['origin']['station']['metaData']['MI']['code'])
     
     @property
     def destination(self) -> Station:
         "Return station object of travel destination"
-        if 'destination' not in self.data:
+        if 'destination' not in self._data:
             raise ValueError('No destination key in travel data')
-        if 'station' not in self.data['destination']:
+        if 'station' not in self._data['destination']:
             raise ValueError('No station key in travel data[destination]')
-        if 'metaData' not in self.data['destination']['station']:
+        if 'metaData' not in self._data['destination']['station']:
             raise ValueError('No metaData key in travel data[destination][station]')
-        if 'MI' not in self.data['destination']['station']['metaData']:
+        if 'MI' not in self._data['destination']['station']['metaData']:
             raise ValueError('No MI key in travel data[destination][station][metaData]')
-        if 'code' not in self.data['destination']['station']['metaData']['MI']:
+        if 'code' not in self._data['destination']['station']['metaData']['MI']:
             raise ValueError('No MI key in travel data[destination][station][metaData][MI]')
-        return Station(self.data['destination']['station']['metaData']['MI']['code'])
+        return Station(self._data['destination']['station']['metaData']['MI']['code'])
 
     @property
     def duration(self) -> int:
         "return duration in second"
-        if 'duration' not in self.data:
+        if 'duration' not in self._data:
             raise ValueError('No duration key in travel data')
-        return int(self.data['duration'])
+        return int(self._data['duration'])
 
     @property
     def functional_id(self) -> str:
         "return functionalId of the travel"
-        if 'functionalId' not in self.data:
+        if 'functionalId' not in self._data:
             raise ValueError('No functionalId key in travel data')
-        return int(self.data['functionalId'])
+        return int(self._data['functionalId'])
 
     @property
     def uuid(self) -> str:
         "return uuid of the travel"
-        if 'id' not in self.data:
+        if 'id' not in self._data:
             raise ValueError('No id key in travel data')
-        return int(self.data['id'])
+        return int(self._data['id'])
     
     
--- tgvmax_api/models/travel_offer.py
+++ tgvmax_api/models/travel_offer.py
@@ -1,4 +1,33 @@
+from typing import TypeVar
 
+
+Travel = TypeVar('Travel')
 
 class TravelOffer:
-    pass
+    
+    def __init__(self, data: dict, travel: Travel):
+        self._data = data
+        self._travel = travel
+
+    @property
+    def id(self) -> str:
+        if 'id' not in self._data:
+            raise ValueError('No id key in travel offer data')
+        return self._data['id']
+
+    @property
+    def amount(self) -> int:
+        if 'amount' not in self._data:
+            raise ValueError('No amount key in travel offer data')
+        return self._data['amount']
+
+    @property
+    def travelClass(self) -> int:
+        if 'travelClass' not in self._data:
+            raise ValueError('No travelClass key in travel offer data')
+        if self._data['travelClass']=='1ère':
+            return 1
+        elif self._data['travelClass']=='2nde':
+            return 2
+        else:
+            raise ValueError('TravelClass is neither first or second class')
